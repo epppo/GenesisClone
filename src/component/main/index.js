69a664305491be8f1e509631323f0cb3fefd719d
epppo <amorosso@kakao.com>
2022-08-22 00:49:27 +0900

    #3 ServiceApply 작업 완

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -1,5 +1,6 @@
 import RelationContents from "./RelationContents"
 import Event from "./Event"
 import AddInfo from "./AddInfo"
+import ServiceApply from "./ServiceApply"
 
-export {RelationContents, Event, AddInfo}
+export {RelationContents, Event, AddInfo, ServiceApply}
